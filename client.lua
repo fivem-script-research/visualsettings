b4e835e42d5515082a6991343fdae8382b6f0f4b
Alex Licuriceanu <alex.licuriceanu@gmail.com>
2026-04-19 18:13:14 +0300

    chore: added manifest and base files

---
+++ client.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'L1CKS'
+description 'Simulate visualsettings.dat mounting and modifying'
+version '1.0.0'
+
+files {
+    "visualsettings.dat"
+}
+
+client_scripts {
+    "client.lua"
+}
